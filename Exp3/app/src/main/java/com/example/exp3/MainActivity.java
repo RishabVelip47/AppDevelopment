d75e7f98ff3fba772ddb58f772f7134446f9fd7a
RishabVelip47 <rishabvelip30582@gmail.com>
2025-09-19 05:55:15 +0530

    Third2

--- Exp3/app/src/main/java/com/example/exp3/MainActivity.java
+++ Exp3/app/src/main/java/com/example/exp3/MainActivity.java
@@ -1,9 +1,10 @@
-package com.example.yourapp;
+package com.example.exp3;
 
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
@@ -17,24 +18,23 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Link UI elements
         textCounter = findViewById(R.id.textCounter);
         btnIncrement = findViewById(R.id.btnIncrement);
         btnReset = findViewById(R.id.btnReset);
 
-        // Increment button logic
+        // Increment button
         btnIncrement.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
+            public void onClick(View v) {
                 counter++;
                 textCounter.setText(String.valueOf(counter));
             }
         });
 
-        // Reset button logic
+        // Reset button
         btnReset.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
+            public void onClick(View v) {
                 counter = 0;
                 textCounter.setText(String.valueOf(counter));
             }
